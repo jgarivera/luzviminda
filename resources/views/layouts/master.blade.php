1afbaebb6b15782f83878cc9496135641ed6caa8
JG Rivera <rivera.jga10@gmail.com>
2021-02-12 15:44:42 +0800

    Removed welcome files

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Header section -->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        @section('head')
+        @show
+    </head>
+
+    <!-- Body section -->
+    <body>
+        @section('body')
+        @show
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('head')
+    <h1>Luzviminda Home</h1>
+@endsection
+
+@section('body')
+    <p>Body section</p>
+@endsection
