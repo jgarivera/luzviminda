5b6e35aa667705d4cdf7e95492ffbb1e383ac91e
DesireeCoder <60638686+DemeterCaubang@users.noreply.github.com>
2021-02-13 21:37:43 +0800

    Landing Page front end prototype conversion

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,13 +5,19 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+        <title>Landing Page</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
         @section('head')
         @show
     </head>
 
     <!-- Body section -->
     <body>
+        <img src="{{ asset('css/Luz-Logo.png') }}"><h2>LuzViMinda</h2>
+            <div class="links">
+                <a href="">Home</a>
+                <a href="">Partners</a> 
+            </div>
         @section('body')
         @show
     </body>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.master')
 
 @section('head')
-    <h1>Luzviminda Home</h1>
+
 @endsection
 
 @section('body')
-    <p>Body section</p>
+    <div class="content">
+        <h1>News</h1>
+        <blockquote class="twitter-tweet"><p lang="tl" dir="ltr">Bilang tugon sa mga tawag at pagpapahayag ng kagustuhan ng mga kababayan natin na magbigay at magpadala ng tulong sa mga nasalanta ni Bagyong ROLLY, activated nang muli ang ating Donations line and focal person. Paalala lang po, mag-iingat sa mga bogus solicitation. <a href="https://t.co/31FyDrXiKF">pic.twitter.com/31FyDrXiKF</a></p>&mdash; NDRRMC (@NDRRMC_OpCen) <a href="https://twitter.com/NDRRMC_OpCen/status/1323838129472638977?ref_src=twsrc%5Etfw">November 4, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+<!-- https://help.twitter.com/en/using-twitter/how-to-embed-a-tweet -->
+        <iframe src="https://observablehq.com/@richdayandnight/untitled?collection=@richdayandnight/one-chart-a-day/2" width="50%" height="300" style="border:none;" center>
+        </iframe>
+        <a href="" class="dash">Go to DashBoard</h3>
+        <a href=""class="login">Login</a>
+    </div>
 @endsection
