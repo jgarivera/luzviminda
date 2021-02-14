edfbca7888102a3696b923d46e6cefd6d2da1860
jay1-53 <sadaanton@student.apc.edu.ph>
2021-02-14 22:57:58 +0800

    Navigation bar hotfix and viewports

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,12 +14,23 @@
     </head>
 
     <!-- Body section -->
-    <body>
-        <img src="{{ asset('css/Luz-Logo.png') }}"><h2>LuzViMinda</h2>
-            <div class="links">
-                <a href="">Home</a>
-                <a href="">Partners</a> 
+    <body class="uk-height-viewport" style="padding: 25px;">
+        <nav class="uk-navbar" id="navbar" data-uk-navbar="">
+            <div class="uk-navbar-left">
+                <a style="color: #fff" class="uk-navbar-item uk-logo" href="#"><img src="{{ asset('css/Luz-Logo.png') }}" />Luzviminda</a>
             </div>
+            
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav uk-visible@m">
+                    <li><a style="color: #fff" href="#">Home</a></li>
+                    <li><a style="color: #fff" href="#">Partners</a></li>
+                </ul>
+                <!-- <div class="uk-navbar-item">
+                    <a class="uk-navbar-toggle uk-hidden@m uk-icon uk-navbar-toggle-icon" data-uk-toggle="" data-uk-navbar-toggle-icon="" href="#offcanvas-nav" aria-expanded="false"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><rect y="9" width="20" height="2"></rect><rect y="3" width="20" height="2"></rect><rect y="15" width="20" height="2"></rect></svg></a>
+                    <a href="#" class="uk-button uk-button-secondary uk-visible@m"><span data-uk-icon="sign-in" class="uk-icon"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><polygon points="7 2 17 2 17 17 7 17 7 16 16 16 16 3 7 3"></polygon><polygon points="9.1 13.4 8.5 12.8 11.28 10 4 10 4 9 11.28 9 8.5 6.2 9.1 5.62 13 9.5"></polygon></svg></span>SIGN UP</a>
+                </div> -->
+            </div>
+        </nav>
         @section('body')
         @show
     </body>
