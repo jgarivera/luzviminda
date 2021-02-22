168e089573858c2ee4934a56db95b3b32cdded5c
estibun <estibun@pm.me>
2021-02-22 21:07:35 +0800

    Improved UI of hovering

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -33,10 +33,36 @@
             background-color: #fff;
             box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
             border-radius: 3px;
-            padding: 10px;
+            padding: 20px;
             margin-bottom: 10px;
+            width: 250px;
         }
-        
+        #ov-province{
+            margin-top: -20px;
+            font-weight: bold;
+        }
+        #ov-municity{
+            text-decoration: underline;
+            margin-top: -20px;
+        }
+        #ov-gradient{
+            width: 150px;
+            margin-top: -10px;
+            height: 10px;
+            background-image: linear-gradient(to right, #dc2727, #f8b20c, #48e74f);
+        }
+        .ov-essinfo{
+            text-indent: 20px;
+        }
+        #donoamount, #affected{
+            margin-top: -20px;
+        }
+        #donodrive, #donoamount, #affected{
+            font-size: medium;
+        }
+        #donodrive{
+            margin-top: 10px;
+        }
     </style>
 @endsection
 
@@ -44,15 +70,17 @@
     <!-- Map container -->
     <div id="map"></div>
 
-    <div class="map-overlay top">
+    <div class="map-overlay top" style="stroke: black">
         <div class="map-overlay-inner">
-            <p>You are currently hovering...</p>
+            <p><i>You are currently hovering...</i> </p>
             <h3 id="ov-province">Province</h3>
             <h4 id="ov-municity">Municity</h4>
-
-            <h4>No. of donation drives: <span id="ov-donodrive">0</span></h4>
-            <h4>Total donations: <span id="ov-donoamount">0</span></h4>
-            <h4>No. of affected people: <span id="ov-affected">0</span></h4>
+            <div id="ov-gradient"></div>
+            <div class="ov-essinfo">
+                <h4 id="donodrive">No. of donation drives: <span id="ov-donodrive">0</span></h4>
+                <h4 id="donoamount">Total donations: <span id="ov-donoamount">0</span></h4>
+                <h4 id="affected">No. of affected people: <span id="ov-affected">0</span></h4>
+            </div>
         </div>
     </div>
 @endsection
