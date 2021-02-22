2628bed0e71b8496037084f704c7cb2c44d80c96
estibun <estibun@pm.me>
2021-02-22 21:06:54 +0800

    Reponsiveness of other organization's card

--- resources/views/pages/confirmationpage.blade.php
+++ resources/views/pages/confirmationpage.blade.php
@@ -24,23 +24,23 @@
 
         <center><h1 style="color:white;">Other Pages</h1></center>
 
-        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-            <div class="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-                <center><h3 class="uk-card-title">Learn more about ResilientPH donation drives here</h3>
+        <div class="uk-child-width-expand@s uk-text-center" uk-grid>
+            <div class="uk-card uk-card-secondary uk-card-body">
+                <h3 class="uk-card-title">Learn more about ResilientPH donation drives here</h3>
                 <div class="thumbnail" style="background-image: url(images/resilient ph logo.jpg);"> </div>
-                <a href="orgprofile" class="form-btns">Go to Page</a></center>
+                <a href="orgprofile" class="form-btns">Go to Page</a>
             </div>
             
-            <div class="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-                <center><h3 class="uk-card-title">Learn more about other Partner Organizations here</h3>
+            <div class="uk-card uk-card-secondary uk-card-body">
+                <h3 class="uk-card-title">Learn more about other Partner Organizations here</h3>
                 <div class="thumbnail" style="background-image: url(images/volunteers.jpg);"> </div>
-                <a href="partnerorg" class="form-btns">Go to Page</a></center>
+                <a href="partnerorg" class="form-btns">Go to Page</a>
             </div>
 
-            <div class="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-                <center><h3 class="uk-card-title">Learn more about Disaster Resiliency here</h3>
+            <div class="uk-card uk-card-secondary uk-card-body">
+                <h3 class="uk-card-title">Learn more about Disaster Resiliency here</h3>
                 <div class="thumbnail" style="background-image: url(images/NDRM.png);"> </div>
-                <a href="/" class="form-btns">Go to Page</a></center>
+                <a href="/" class="form-btns">Go to Page</a>
             </div>
         </div>
     </div>
