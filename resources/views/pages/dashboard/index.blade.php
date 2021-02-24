1916a812a279a9389561fab2b6d0e581f7ef3de1
JG Rivera <rivera.jga10@gmail.com>
2021-02-24 16:12:41 +0800

    Integrated the mockup statistics layout

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,19 +1,32 @@
 @extends('layouts.master-dashboard')
 
 @section('head')
-    <title>Landing Page</title>
+    <title>Dashboard</title>
+
+    <!-- Load scripts -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" defer></script>
     <script src="{{ asset('js/charts.js') }}" defer></script>
 
-    <link rel="stylesheet" href="{{ asset('css/dashboard/charts.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard/home.css') }}">
 @endsection
 
 @section('body')
-    <div class="chart-container">
-        <canvas id="donationChart"></canvas>
-    </div>
 
-    <div class="chart-container">
-        <canvas id="affectedPeopleChart"></canvas>
+    <!-- Statistics -->
+    <div class="statistics">
+        <h2>Statistics</h2>
+        <div class="row">
+            <div class="column">
+                <div class="chart-container">
+                    <canvas id="donationChart"></canvas>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="chart-container">
+                    <canvas id="affectedPeopleChart"></canvas>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
