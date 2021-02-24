e9e176681647a6d8f958312f1e253fbf5b512a9d
JG Rivera <rivera.jga10@gmail.com>
2021-02-24 15:46:13 +0800

    Integrated chart.js into dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -2,8 +2,18 @@
 
 @section('head')
     <title>Landing Page</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" defer></script>
+    <script src="{{ asset('js/charts.js') }}" defer></script>
+
+    <link rel="stylesheet" href="{{ asset('css/dashboard/charts.css') }}">
 @endsection
 
 @section('body')
-    The body
+    <div class="chart-container">
+        <canvas id="donationChart"></canvas>
+    </div>
+
+    <div class="chart-container">
+        <canvas id="affectedPeopleChart"></canvas>
+    </div>
 @endsection
