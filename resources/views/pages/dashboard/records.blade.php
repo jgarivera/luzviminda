508f388e16c3a97e36845bf019df74f6a57bf835
JG Rivera <rivera.jga10@gmail.com>
2021-02-26 15:25:10 +0800

    Added rough implementation of DataTables

--- resources/views/pages/dashboard/records.blade.php
+++ resources/views/pages/dashboard/records.blade.php
@@ -3,8 +3,45 @@
 @section('head')
     <title>Records Manager</title>
 
+    <!-- DataTables CDN -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.js" type="text/javascript" charset="utf8"></script>
+
+    <script src="{{ asset('js/tables.js') }}" defer></script>
 @endsection
 
 @section('body')
-    The body
+    <table id="donations" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Donor</th>
+                <th>Drive</th>
+                <th>Payment Type</th>
+                <th>Amount</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Honda</td>
+                <td>Edinburgh</td>
+                <td>G-Cash</td>
+                <td>100.00</td>
+                <td>In Progress</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>#</th>
+                <th>Donor</th>
+                <th>Drive</th>
+                <th>Payment Type</th>
+                <th>Amount</th>
+                <th>Status</th>
+            </tr>
+        </tfoot>
+    </table>
 @endsection
