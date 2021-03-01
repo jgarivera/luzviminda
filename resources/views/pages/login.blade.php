f15f91fe2700097de023c59ffda67cb961e6a34a
jay1-53 <steven.daanton@gmail.com>
2021-03-02 05:03:59 +0800

    Omitted unnecessary comments

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -80,31 +80,4 @@
         <!-- action buttons -->
     </div>
 </div>
-<!-- <div class="uk-card uk-card-secondary uk-card-body uk-width-1-2@m">
-<center><img src="{{ asset('images/Luz-logo.png') }}" style="width:100px;" /></center>
-    <h3 class="uk-card-title">Welcome to LuzViMinda</h3>
-    
-    <form>
-    <fieldset class="uk-fieldset">
-
-        <div class="uk-margin">
- 
-        <label class="uk-form-label" for="form-horizontal-text">NGO Account:</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-horizontal-text" type="text">
-        </div>
-
-        <label class="uk-form-label" for="form-horizontal-text">Password:</label>
-        <div class="uk-form-controls">
-            <input class="uk-input" id="form-horizontal-text" type="password">
-        </div>
-        
-        </div>
-
-    </fieldset>
-
-    </form>
-
-    <center><a href="{{ route('dashboard.index') }}" class="form-btns">Login</a></center>
-</div> -->
 @endsection
